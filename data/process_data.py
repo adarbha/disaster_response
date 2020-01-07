61fc79f7649e909cc8c1975fe30513013bc2b3ba
root <arjunsdarbha@gmail.com>
2020-01-07 21:17:23 +0000

    Change in save_data to accept db path

--- data/process_data.py
+++ data/process_data.py
@@ -47,8 +47,8 @@ def clean_data(df):
 
 def save_data(df, database_filename):
     '''Saving to sqlite database DisasterResponse.db'''
-    engine = create_engine('sqlite:///DisasterResponse.db')
-    df.to_sql(database_filename, engine, index=False)
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    df.to_sql('msg_cat', engine, index=False)
 
 
 def main():

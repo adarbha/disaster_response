20cdfd3f8a0bb38f7cd4f1ca23663da01fb80a66
root <arjunsdarbha@gmail.com>
2020-01-07 20:32:27 +0000

    Added load_data functionality

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,12 +1,91 @@
 import sys
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import re
+import nltk
+import pycountry
 
+from sqlalchemy import create_engine
+
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
+
+from sklearn.metrics import confusion_matrix, classification_report, f1_score, roc_auc_score, recall_score
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, AdaBoostClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.pipeline import Pipeline, FeatureUnion
+
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.linear_model import LogisticRegression
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
 
 def load_data(database_filepath):
-    pass
+    '''Loads data table from SQLite db and returns X, Y and category names'''
+    engine = create_engine('sqlite:///DisasterResponse.db')
+    df = pd.read_sql_table(database_filepath, engine)
+    
+    ## Remove column original/child_alone as it doesn't add any value. Child alone has zero variance
+    df = df.drop(columns = ['original','child_alone'])
+    
+    ##Dropping whole rows of NaNs
+    df = df.dropna(axis = 0)
+    
+    X = df['message']
+    Y = df.iloc[:,3:]
+    category_names = list(Y.columns)
+    
+    return (X, Y, category_names)
 
 
 def tokenize(text):
-    pass
+    # Remove urls
+    url_regex = r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+    text = re.sub(url_regex, "url_placeholder", text)
+    
+    # Normalize case
+    text = text.lower()
+    
+    # tokenize text
+    tokens = word_tokenize(text)
+    
+    # Lemmatize remove stop words and stem
+    stop_words = stopwords.words("english")
+    lemmatizer = WordNetLemmatizer()
+    stemmer = PorterStemmer()
+    
+    tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stop_words]
+    
+    return tokens
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+    '''Transformer class to check if the first word is a verb'''
+
+    def starting_verb(self, text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            if len(pos_tags) == 0:
+                return False
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return True
+        return False
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
+
 
 
 def build_model():
